ed8f27fa49f13e0e74223c5521b5017318966be0
aleksoider <aleksoider@mail.ru>
2016-01-30 19:27:34 +0300

    frame added

---
+++ app/src/main/java/sav/alex/vkmp/adapters/PlayListAdapter.java
@@ -0,0 +1,7 @@
+package sav.alex.vkmp.adapters;
+
+/**
+ * Created by 123 on 30.01.2016.
+ */
+public class PlayListAdapter {
+}
---
+++ app/src/main/java/sav/alex/vkmp/classes/PlayList.java
@@ -0,0 +1,31 @@
+package sav.alex.vkmp.classes;
+
+import sav.alex.vkmp.intefaces.IPlayList;
+
+/**
+ * Created by 123 on 30.01.2016.
+ */
+public class PlayList implements IPlayList {
+
+
+
+    @Override
+    public String getTrackAt(int id) {
+        return null;
+    }
+
+    @Override
+    public String nextTrack() {
+        return null;
+    }
+
+    @Override
+    public String previousTrack() {
+        return null;
+    }
+
+    @Override
+    public String sourceType() {
+        return null;
+    }
+}
